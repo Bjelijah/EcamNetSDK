2b62cfd6f142cb67b7cc4c40669c572b4cfd1361
Bjelijah <elijah@live.cn>
2017-08-03 15:17:18 +0800

    new texture view

---
+++ app/src/main/java/com/demo/ecamnetsdk/view/PlayGLTextureView.java
@@ -0,0 +1,31 @@
+package com.demo.ecamnetsdk.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.demo.player.gles.GLESRendererImpl;
+
+
+/**
+ * Created by Administrator on 2017/6/23.
+ */
+
+public class PlayGLTextureView extends ZoomableTextureView {
+
+    private Context mContext;
+    GLESRendererImpl mRenderer;
+    public PlayGLTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+        mRenderer = new GLESRendererImpl(context,this,null);
+        setRenderer(mRenderer);
+        setRenderMode(RENDERMODE_WHEN_DIRTY);
+    }
+
+    @Override
+    public void onDestroy() {
+        mRenderer.rendererRelease();
+        super.onDestroy();
+
+    }
+}
